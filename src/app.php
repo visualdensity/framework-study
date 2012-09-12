0d4bd7eea8ec939e16604a22e20c5de86350610d
Wee Keat Chin <weekeat@visualdensity.com>
2012-09-12 16:01:57 +1000

    Part 5 - Refactoring to controller using anonymous functions

--- src/app.php
+++ src/app.php
@@ -1,9 +1,35 @@
 <?php
 
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+
+function is_leap_year($year) {
+    if( null == $year ) {
+        $year = date('Y');
+    } 
+
+    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
 
 $routes = new Routing\RouteCollection();
 $routes->add( 'hello', new Routing\Route( '/hello/{name}', array('name' => 'world')) );
 $routes->add( 'bye'  , new Routing\Route( '/bye' ) );
+
+$routes->add( 
+    'is_leap_year', 
+    new Routing\Route( 
+        '/is_leap_year/{year}',
+        array(
+            'year' => null,
+            '_controller' => function($request) {
+                if( is_leap_year($request->attributes->get('year')) ) {
+                    return new Response('Yes it is');
+                } 
+                return new Response('Nope');
+            }
+        )
+    ) 
+);
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -6,6 +6,14 @@ use Symfony\Component\HttpFoundation\Request,
     Symfony\Component\HttpFoundation\Response,
     Symfony\Component\Routing;
 
+function render_template($request) {
+    extract( $matcher->match($request->getPathInfo()), EXTR_SKIP );
+    ob_start();
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+    $response = new Response( ob_get_clean() );
+}
+
 //create a request object and get routes
 $request = Request::createFromGlobals();
 $routes  = include __DIR__ . '/../src/app.php';
@@ -18,11 +26,9 @@ $request_context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $request_context);
 
 try {
-    extract( $matcher->match($request->getPathInfo()), EXTR_SKIP );
-    ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-    $response = new Response( ob_get_clean() );
+    //add the matched results to $request->attributes
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 }
 catch ( Routing\Exception\ResourceNotFoundException $e ) {
     $response = new Response( 'Where?!', 404 );
